67a3156a6eab3ef09006708d211370179c15038b
rachealkuranchie <kuranchieracheal@gmail.com>
2024-11-14 14:19:29 +0000

    fixed config to allow port running

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',  // Listen on all IP addresses
+    port: 5173,       // Optional: Specify port (default is 5173)
+    strictPort: true, // Optional: Prevent port fallback
+  }
 })
