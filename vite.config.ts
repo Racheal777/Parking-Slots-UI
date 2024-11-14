75fabb685e7de5abe008a6c7e22d347a12a5778b
rachealkuranchie <kuranchieracheal@gmail.com>
2024-11-14 15:00:08 +0000

    fixes

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: '0.0.0.0',  // Listen on all IP addresses
-    port: 5173,       // Optional: Specify port (default is 5173)
-    strictPort: true, // Optional: Prevent port fallback
+    // port: 8080,       // Optional: Specify port (default is 5173)
+    // strictPort: true, // Optional: Prevent port fallback
   }
 })
